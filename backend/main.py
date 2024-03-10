a69fc17417c9db7566b4db7ebf16b5a3b982a172
Abdul-Maajith <abdulmaajith0703@gmail.com>
2024-03-10 17:41:26 +0530

    First commit

---
+++ backend/main.py
@@ -0,0 +1,67 @@
+from typing import List
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
+import json
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class ConnectionManager:
+
+    def __init__(self) -> None:
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+# define endpoint
+
+
+@app.get("/welcome")
+def Home():
+    return "Welcome home"
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to AgentStack API Docs"}
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(f"Data from UI: {data}")
+            message = {"time":current_time,"clientId":client_id,"message":data}
+            print(f"Message from Backend: {message}")
+            await manager.send_personal_message(json.dumps(message), websocket)
+            
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        message = {"time":current_time,"clientId":client_id,"message":"Offline"}
+        await manager.broadcast(json.dumps(message))
